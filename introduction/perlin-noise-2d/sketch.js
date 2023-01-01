c3a73161e8507dd839e6215f1452e8fc849bca5c
carlynorama <carlynorama@gmail.com>
2022-12-31 19:51:22 -0800

    2D Example

--- introduction/perlin-noise-2d/sketch.js
+++ introduction/perlin-noise-2d/sketch.js
@@ -1,25 +1,32 @@
-var inc = 0.01;
+//see https://editor.p5js.org/codingtrain/sketches/2_hBcOBrF
+
+let inc = 0.01;
 
 function setup() {
   createCanvas(200, 200);
+  pixelDensity(1);
+  noiseDetail(8, 0.5);
 }
 
 function draw() {
-  loadPixels();
-  background(51);
-  stroke(255);
-  noFill();
-  beginShape();
-  let = yoff = start;
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      var index = (x + y*width) * 4;
-      pixels[index + 0] = 255;
-      pixels[index + 1] = 0;
-      pixels[index + 2] = 0;
-      pixels[index + 4] = 255;
-    }
-  }
 
+  loadPixels();
+  let yoff = 0;
+  for (let y = 0; y < height; y++) {
+    let xoff = 0;
+    for (let x = 0; x < width; x++) {
+      let index = (x + y * width) * 4;
+      // let r = random(255);
+      let r = noise(xoff, yoff) * 255;
+      pixels[index + 0] = r;
+      pixels[index + 1] = r;
+      pixels[index + 2] = r;
+      pixels[index + 3] = 255;
+
+      xoff += inc;
+    }
+    yoff += inc;
+  }
   updatePixels();
+  //noLoop();
 }
