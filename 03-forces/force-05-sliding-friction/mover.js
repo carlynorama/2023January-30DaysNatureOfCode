439817d867850d6d7ac37b3291fa1b02161ab067
carlynorama <carlynorama@gmail.com>
2023-01-02 22:58:40 -0800

    sliding friction

--- 03-forces/force-05-sliding-friction/mover.js
+++ 03-forces/force-05-sliding-friction/mover.js
@@ -1,5 +1,5 @@
 class Mover {
-  constructor(x, y, m, c) {
+  constructor(x, y, m, c, g) {
     // this.startx = x;
     // this.starty = y;
     this.pos = createVector(x, y);
@@ -18,15 +18,31 @@ class Mover {
     this.hBounds = createVector(r, width - r);
     this.vBounds = createVector(r, height - r);
 
-    this.wallDeceleration = 0.95;
+    // this.lasty = 0;
+    // this.ystill = false;
 
+    //WORLD VALUES
+    this.wallDeceleration = 0.90;
+    this.gravity = g;
+    this.weight = p5.Vector.mult(g, this.mass);
+  }
+
+  updateGravity(gravity) {
+    this.weight = p5.Vector.mult(gravity, this.mass);
+  }
+
+  applyGravity() {
+    this.acceleration.add(this.gravity);
   }
 
   update() {
+
     this.velocity = this.checkEdges(this.pos, this.velocity, this.hBounds, this.vBounds, this.wallDeceleration);
     if (this.pos.y < this.vBounds.x) {
       this.pos.y = this.vBounds.x
+      //console.log("snapping")
     } else if (this.pos.y > this.vBounds.y) {
+      //console.log("snapping")
       this.pos.y = this.vBounds.y
     }
     if (this.pos.x < this.hBounds.x) {
@@ -35,15 +51,51 @@ class Mover {
       this.pos.x = this.hBounds.y
     }
 
+
+    this.applyGravity();
     this.velocity.add(this.acceleration);
+
     this.pos.add(this.velocity);
+    // if (abs(this.pos.y-this.lasty) < 0.1 ) {
+    //   this.ystill = true;
+    //   //this.velocity.y = 0;
+    //   //console.log("Ive stopped:", this.velocity.y, this.weight.y, this.acceleration.y)
+    // } else {
+    //   this.ystill = false;
+    // }
+    //console.log("comparebeforeClear:", this.weight.y, this.mass, this.acceleration.y)
     this.clearExternalForces();
+    //this.lasty = this.pos.y;
   }
 
   applyForce(force) {
     let f = p5.Vector.div(force, this.mass);
     this.acceleration.add(f);
   }
+
+  applyFriction(mu) {
+    let horizontalFriction = this.velocity.copy().normalize().mult(-1);
+
+    //horizontal lines don't apply y friction
+    horizontalFriction.y = 0;
+    horizontalFriction.mult((mu * this.weight.y));
+
+    this.acceleration.add(horizontalFriction);
+  }
+
+  applyFriction2(mu) {
+    let friction = this.velocity.copy().normalize().mult(-1);
+    let weightMu = this.weight.copy().mult(mu);
+
+    friction.x *= weightMu.y;
+    friction.y *= weightMu.x;
+    //horizontal lines don't apply y friction
+
+
+    this.acceleration.add(friction);
+  }
+
+
 
   clearExternalForces() {
     //tmp - no internal motivation
@@ -66,9 +118,17 @@ class Mover {
     return newVelocity
   }
 
+  isSliding() {
+    //console.log("compare2:", this.weight.y, this.acceleration.y)
+    //this.color = 50;
+    //
+    //return  (this.ystill || (this.velocity.x > 2*this.velocity.y) ) && (this.pos.y >= this.vBounds.y);
+    return (this.pos.y >= this.vBounds.y);
+  }
+
   render() {
     fill(this.color);
-    noStroke();
+    stroke(153);
     //strokeWeight(2);
     ellipse(this.pos.x, this.pos.y, this.diameter, this.diameter);
     //ellipse(this.x, this.y, this.diameter, this.diameter);
