9beb62bae1bd2563cbed832db0b3af80fa69109a
carlynorama <carlynorama@gmail.com>
2023-01-04 15:21:00 -0800

    start quad tree

--- 05-quadtree/step02_start_quad_tree/quadtree.js
+++ 05-quadtree/step02_start_quad_tree/quadtree.js
@@ -1,58 +1,24 @@
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-class Size {
-  constructor(w, h) {
-    this.width = w;
-    this.height = h;
-  }
-}
-
-class Range {
-  constructor(l, u) {
-    this.lowerbounds = l;
-    this.upperbounds = u;
-  }
-
-  inclusiveContains() {
-
-  }
-
-  exclusiveContains() {
-
-  }
-
-  includeLowerContains() {
-
-  }
-
-  includeUpperContains() {
-
-  }
-}
-
-class Bounds {
-  constructor(x, y, w, h) {
-    this.origin = new Point(x,y);
-    this.size = new Size(w,h);
-  }
-
-  contains(x, y) {
-    let result = false;
-
-    return result;
-  }
-
-  contains(point) {
-    return contains(x,y);
-  }
-}
-
-
 class QuadTree {
+  constructor(bounds, count) {
+    this.bounds = bounds;
+    this.count = count;
+    this.points = [];
+  }
+
+  addPoint(x,y) {
+    let point = new Point(x,y);
+    return addPoint(point);
+  }
+
+  addPoint(point) {
+
+    if (!this.bounds.contains(point)) {
+      return false;
+    } else {
+      this.points.push(point);
+    }
+
+
+  }
 
 }
