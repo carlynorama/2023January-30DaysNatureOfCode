486ad5ec3ad7c29cef157012c04e1631b8645750
carlynorama <carlynorama@gmail.com>
2023-01-03 17:02:32 -0800

    works

--- 04-forces/force2-02-correctedvalues/sketch.js
+++ 04-forces/force2-02-correctedvalues/sketch.js
@@ -4,7 +4,7 @@ let numMovers = 12;
 let liquidBoundary = 0;
 //https://sciencenotes.org/table-of-density-of-common-materials/
 let airRho = 0.0001;
-let liquidRho = airRho * 10; //(water is about 1000 times more dense than air)
+let liquidRho = airRho * 1000; //(water is about 1000 times more dense than air)
 
 
 function setup() {
@@ -43,6 +43,10 @@ function draw() {
     movers.forEach(mover => {
        if (mover.pos.y > liquidBoundary) {
          mover.color = color3;
+         //energy dissipates in sound, etc. TODO RealEQ
+         //When the delta is 100x, this value needs to be at MAX 0.30;
+         //When the delta is 1000x, this value needs to be ~0.1;
+         mover.velocity.y *= 0.09;
          mover.applyDrag(liquidRho);
         } else {
           mover.color = color1;
@@ -53,7 +57,7 @@ function draw() {
        if (mover.pos.y > height-(mover.diameter/2)) {
          mover.color = color2;
          mover.applyReverseGravity();
-         mover.velocity.y *= 0.90; //the items experiencing less drag will burry into the bottom.
+         //mover.velocity.y *= 0.90; //the items experiencing less drag will burry into the bottom.
        }
 
 
