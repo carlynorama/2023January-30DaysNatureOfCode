88d8a871aea967665791556f77592860f5bff3ff
carlynorama <carlynorama@gmail.com>
2023-01-05 09:14:10 -0800

    contain miss matches?

--- 05-quadtree/step05_add_subtrees/quadtree.js
+++ 05-quadtree/step05_add_subtrees/quadtree.js
@@ -6,9 +6,16 @@ class QuadTree {
     this.subTrees = []; //[ne,se,sw,nw];
   }
 
+  static createEmptyTree(bounds, count) {
+    this.bounds = bounds;
+    this.limit = count;
+    this.points = [];
+    this.subTrees = []; //[ne,se,sw,nw];
+  }
+
   addPoint(x, y, pointSuccess) {
     if (!this.contains(x,y)) {
-      console.log(x,y,"not in here")
+      console.log(x,y,"not in here", this.bounds.minX(), this.bounds.maxX(), this.bounds.minY(), this.bounds.minY())
       return false;
     } else {
       if (this.subTrees.length != 0) {
@@ -40,40 +47,48 @@ class QuadTree {
 
   //this is only called if point is KNOWN to be in the bounds. It should
   //never faile to find a home.
+  // addPointToSubTree(x,y) {
+  //   let west = new Range(this.bounds.origin.x,this.bounds.size.width/2);
+  //   let north = new Range(this.bounds.origin.y, this.bounds.size.height/2);
+  //   //<-- ! IMPORTANT this is the same type of check as Bounds uses
+  //   let isWest = west.upperInclusiveContains(x);
+  //   let isNorth = north.upperInclusiveContains(y);
+  //
+  //
+  //   if (isNorth && !isWest) {
+  //     let tree = this.subTrees[0];
+  //     //console.log(tree.bounds.origin.x);
+  //     return tree.addPoint(x,y);
+  //   } else if (!isNorth && !isWest) {
+  //     let tree = this.subTrees[1];
+  //     //console.log(tree.bounds.origin.x);
+  //     return tree.addPoint(x,y);
+  //   } else if (!isNorth && isWest) {
+  //     let tree = this.subTrees[2];
+  //     //console.log(tree.bounds.origin.x);
+  //     return tree.addPoint(x,y);
+  //   } else {
+  //     let tree = this.subTrees[3];
+  //     //console.log(tree.bounds.origin.x);
+  //     return tree.addPoint(x,y);
+  //   }
+  //
+  //   console.log("If statement failure.");
+  //   return false;
+  // }
+
   addPointToSubTree(x,y) {
-    let west = new Range(this.bounds.origin.x,this.bounds.size.width/2);
-    let north = new Range(this.bounds.origin.y, this.bounds.size.height/2);
-    //<-- ! IMPORTANT this is the same type of check as Bounds uses
-    let isWest = west.upperInclusiveContains(x);
-    let isNorth = north.upperInclusiveContains(y);
-
-    if (isNorth && !isWest) {
-      let tree = this.subTrees[0];
-      //console.log(tree.bounds.origin.x);
-      drawBounds(tree);
-      return tree.addPoint(x,y);
-
-    } else if (!isNorth && !isWest) {
-      let tree = this.subTrees[1];
-      //console.log(tree.bounds.origin.x);
-      drawBounds(tree);
-      return tree.addPoint(x,y);
-    } else if (!isNorth && isWest) {
-      let tree = this.subTrees[2];
-      //console.log(tree.bounds.origin.x);
-      drawBounds(tree);
-      return tree.addPoint(x,y);
-    } else {
-      let tree = this.subTrees[3];
-      //console.log(tree.bounds.origin.x);
-      drawBounds(tree);
-      return tree.addPoint(x,y);
+    for (let i = 0; i < 4; i++) {
+      if (this.subTrees[i].contains(x,y)) {
+        console.log("I think you're in here!");
+        return this.subTrees[i].addPoint(x,y);
+      }
     }
-
-    console.log("If statement failure.");
-    return false;
-
+    console.log("Didn't find a home.");
   }
+
+
+
 
   contains(x,y) {
     return this.bounds.contains(x,y);
