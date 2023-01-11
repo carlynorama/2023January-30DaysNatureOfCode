8777c9174b05cf518b28fc3986eebf9cea8e7b11
carlynorama <carlynorama@gmail.com>
2023-01-11 11:48:09 -0800

    moving global

--- src/11-first-day-of-typescript/01-hello-typescript/global.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// This file will add both p5 instanced and global intellisence
-//https://github.com/Gaweph/p5-typescript-starter/blob/master/global.d.ts 
-import * as p5Global from 'p5/global' 
-import module = require('p5');
-export = module;
-export as namespace p5;
