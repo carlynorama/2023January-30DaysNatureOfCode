0a6b0255d5b2fa01602da0145b92128a08e4f13a
carlynorama <carlynorama@gmail.com>
2023-01-12 16:50:57 -0800

    tidy up

--- docs/12-circular-motion/05-polar-mover-object/sketch.js
+++ docs/12-circular-motion/05-polar-mover-object/sketch.js
@@ -6,32 +6,18 @@ let r = 150;
 let direction = 1;
 let moverA;
 let moverB;
-let deflection;
-let a;
-let b;
-let c;
 function setup() {
     createControlledCanvas(400, 400);
     originx = width / 2;
     originy = height / 2;
     moverA = PolarMover.createPolarMover(angle, r);
     moverB = PolarMover.createPolarMover(angle, r);
-    deflection = moverB.position.normalized().perpendicularAngle() + 0.04;
-    // a = Vector.createAngleVector(Math.PI/3, 100);
-    // b = Vector.createAngleVector(Math.PI/6, 100);
-    // c = a.added(b);
     background(51);
 }
 function draw() {
-    let color2 = color(0, 204, 204, 255);
-    //let color3 = color(204);
     if (runFlag) {
-        background(51, 0);
-        stroke(color2);
+        //background(51, 0);
         noFill();
-        strokeWeight(4);
-        ellipseMode(CENTER);
-        //draw polor sends push and pop
         translate(originx, originy);
         moverA.setPosition(angle, r);
         stroke(51, 204, 153, 100);
@@ -44,31 +30,16 @@ function draw() {
         else if (r < 0) {
             direction = -1;
         }
-        // noFill();
-        // strokeWeight(4);
-        // ellipseMode(CENTER);
-        // ellipse(0, 0, r);
-        let degreesIn = (4 * 0.01745329252);
+        //let degreesIn = (4*0.01745329252)
         moverB.updatePosition(moverB.position.normalized().perpendicularAngle() + Math.PI / 64, 1);
         stroke(204);
         moverB.needsCartesian(drawMe);
     }
 }
-function drawVectorAdd(a, b) {
-    push();
-    strokeWeight(1);
-    c = a.added(b);
-    line(0, 0, a.x, a.y);
-    line(a.x, a.y, a.x + b.x, a.y + b.y);
-    line(0, 0, c.x, c.y);
-    pop();
-}
 function drawMe(x, y, a) {
     push();
-    //let color1 =  color(204);//color(51, 204, 153, 100);
-    //stroke(color1);
     strokeWeight(2);
-    let size = 10; //mover.mass;
+    let size = 10;
     push();
     translate(x, y);
     rotate(a);
@@ -78,4 +49,19 @@ function drawMe(x, y, a) {
     point(x, y);
     pop();
 }
+// let a:Vector;
+// let b:Vector;
+// let c:Vector;
+// a = Vector.createAngleVector(Math.PI/3, 100);
+// b = Vector.createAngleVector(Math.PI/6, 100);
+// c = a.added(b);
+// function drawVectorAdd(a:Vector, b:Vector) {
+//   push();
+//   strokeWeight(1);
+//   c = a.added(b);
+//   line(0, 0, a.x, a.y);
+//   line(a.x, a.y, a.x + b.x, a.y + b.y);
+//   line(0, 0, c.x, c.y);
+//   pop();
+// }
 //# sourceMappingURL=sketch.js.map
