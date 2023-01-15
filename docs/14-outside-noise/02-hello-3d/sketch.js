65410ff140d35f64b66f924135779488b8a68fdb
carlynorama <carlynorama@gmail.com>
2023-01-14 17:53:15 -0800

    3d works

--- docs/14-outside-noise/02-hello-3d/sketch.js
+++ docs/14-outside-noise/02-hello-3d/sketch.js
@@ -1,31 +1,39 @@
 import { makeNoise3D } from "../../addons/open-simplex-noise/main.js";
+//../../addons/open-simplex-noise/main.js
 let sketch = function (p) {
-    let a = 100;
-    let b = 100;
+    const a = 100;
+    const b = 100;
     let noiseMaker;
-    let z = 0;
-    let z_inc = 0.01;
+    let zoff = 0;
+    const z_inc = 0.1;
+    const y_inc = 0.1;
+    const x_inc = 0.1;
     p.setup = function () {
         p.createCanvas(200, 200);
         noiseMaker = makeNoise3D(Date.now());
-        p.noLoop();
+        //p.noLoop();
     };
     p.draw = function () {
-        p.background(0);
-        p.fill(255);
+        p.background(100);
+        p.fill(204, 0, 0);
         p.rect(a, b, 50, 50);
+        let xoff = 0;
         for (let x = 0; x < p.width; x++) {
+            let yoff = 0;
             for (let y = 0; y < p.height; y++) {
-                //const i = (x + y * p.width) * 4;
                 //TODO run a check?
-                const value = (noiseMaker(x, y, z) + 1) * 128;
-                console.log(value);
-                //let bright = value > 0 ? 255 : 0;
-                p.stroke(value);
+                // const value = (noiseMaker(xoff, yoff, zoff) + 1) * 128;
+                const value = (noiseMaker(xoff, yoff, zoff));
+                //console.log(value);
+                let bright = value > 0 ? 255 : 0;
+                //p.stroke(value);
+                p.stroke(bright);
                 p.point(x, y);
+                yoff += y_inc;
             }
+            xoff += x_inc;
         }
-        z += z_inc;
+        zoff += z_inc;
     };
 };
 let myp5 = new p5(sketch);
--- src/14-outside-noise/02-hello-3d/sketch.ts
+++ src/14-outside-noise/02-hello-3d/sketch.ts
@@ -7,36 +7,42 @@ import { Noise4D } from "open-simplex-noise/lib/4d";
 //../../addons/open-simplex-noise/main.js
 
 let sketch = function(p:p5) {
-  let a = 100;
-  let b = 100;
+  const a = 100;
+  const b = 100;
   let noiseMaker:Noise3D;
 
-  let z = 0;
-  let z_inc = 0.01;
-
+  let zoff = 0;
+  const z_inc = 0.01;
+  const y_inc = 0.01;
+  const x_inc = 0.01;
   p.setup = function() {
     p.createCanvas(200, 200);
     noiseMaker = makeNoise3D(Date.now());
-    p.noLoop();
+    //p.noLoop();
   };
 
-  p.draw = function() {
+  p.draw = function () {
     p.background(0);
-    p.fill(255);
+    p.fill(204, 0, 0);
     p.rect(a, b, 50, 50);
-      for (let x = 0; x < p.width; x++) {
-          for (let y = 0; y < p.height; y++) {
-              //const i = (x + y * p.width) * 4;
-              //TODO run a check?
-              const value = (noiseMaker(x, y, z) + 1) * 128;
-              console.log(value);
-              //let bright = value > 0 ? 255 : 0;
-              p.stroke(value);
-              p.point(x, y);
-          }
-      }
-      z += z_inc;
-  };
+    let xoff = 0;
+    for (let x = 0; x < p.width; x++) {
+        let yoff = 0;
+        for (let y = 0; y < p.height; y++) {
+          //TODO run a check?
+          // const value = (noiseMaker(xoff, yoff, zoff) + 1) * 128;
+          const value = (noiseMaker(xoff, yoff, zoff));
+          //console.log(value);
+          let bright = value > 0 ? 255 : 0;
+          //p.stroke(value);
+          p.stroke(bright);
+          p.point(x, y);
+          yoff += y_inc;
+        }
+        xoff += x_inc;
+    }
+    zoff += z_inc;
+};
 };
   
 let myp5 = new p5(sketch);
